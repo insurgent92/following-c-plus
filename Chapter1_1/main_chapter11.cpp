8cd770f54a30b033dd91378a9e717f3b401d8e8b
unknown <insurgent92@gmail.com>
2019-03-25 14:45:59 +0900

    Add chapter_1

---
+++ Chapter1_1/main_chapter11.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // '#' stands for preprocessor directive 
+
+int main() // or int main(void), etc... 
+{
+	int x = 2;
+	x = 5;
+	int y = x + 3;
+
+	std::cout << y << std::endl; // '<<' stands for output operator 
+
+	return 0;
+}
