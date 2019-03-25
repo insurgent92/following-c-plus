3435b1c1fb62fa109b396e840d5a1886a56a4ae6
unknown <insurgent92@gmail.com>
2019-03-25 15:25:22 +0900

    Add 1_3

---
+++ following-c-plus/Chapter 1_3/main_1_3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	int x = 1;
+	x = 2 + 2;
+	std::cout << x << std::endl;
+
+	int y = x;
+	std::cout << y << std::endl;
+
+	// is ( x + y ) l-value or r-value ?
+	std::cout << x + y << std::endl;
+
+	std::cout << x << std::endl;
+
+	/*int z;
+	std::cout << z << std::endl;*/
+}
