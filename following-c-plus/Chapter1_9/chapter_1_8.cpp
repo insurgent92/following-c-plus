fb09341607f49bae34581847878f162329feea36
unknown <insurgent92@gmail.com>
2019-03-27 10:26:45 +0900

    Add chap1_9

---
+++ following-c-plus/Chapter1_9/chapter_1_8.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int x, int y) 
+{ 
+	return x + y; // indenting(ºóÄ­¸¸µé±â) 
+}
+
+int main()
+{
+	cout << "Hello, world!" << endl;
+
+	int my_v		= 1;
+	int x			= 4;
+	int num_apples	= 123;
+	//lots of hard coding
+
+	// this is important
+	int x = 1 + 2;  
+
+	// this is important, too
+	int y = 3 + 4; 
+
+	return 0;
+}
