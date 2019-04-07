3200f0c2f354a3af55e8b391b86c5e2f598df48b
visionNoob <insurgent92@kangwon.ac.kr>
2019-04-07 11:18:45 +0900

    Add chap2.8

---
+++ Chapter2_8/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	float pi = 3.14f;
+
+	int i = -1234l;
+
+	unsigned char _uc = 256;
+
+	cout << pi << endl;
+	cout << i << endl;
+	cout << (int)_uc << endl;
+
+	return 0;
+
+}
