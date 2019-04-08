c7c9293dcb3399bf2e242278aa689772a0133f49
visionNoob <insurgent92@kangwon.ac.kr>
2019-04-08 13:44:11 +0900

    c

---
+++ following-c-plus/Chapter3_2/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	int x = 7;
+	int y = 4;
+
+	cout << x / y << endl;
+	cout << x / (double)y << endl;
+	
+	cout << 5 / -2 << endl;
+	cout << 5 % -2 << endl;
+
+	return 0;
+}
